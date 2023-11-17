aa5cf6c71e918c9791af9310dfaa2a07135aa6b3
Lgrella <60951320+Lgrella@users.noreply.github.com>
2023-11-17 13:50:27 +0000

    update execution time code

--- rust_analysis/src/main.rs
+++ rust_analysis/src/main.rs
@@ -23,11 +23,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mean = insurances.par_iter().map(|i| i.charges).sum::<f64>() / insurances.len() as f64;
     let median = insurances[insurances.len() / 2].charges;
     let std_dev = (insurances.par_iter().map(|i| (i.charges - mean).powi(2)).sum::<f64>() / insurances.len() as f64).sqrt();
+    let execution_time = start_time.elapsed().as_secs_f64() * 1000.0;
 
     //let cpu = 0;
     let mem = sys_info::mem_info().unwrap();
     let mem_usage_perc = ((mem.total - mem.avail) as f64 / mem.total as f64) * 100.0;
-    let execution_time = start_time.elapsed().as_secs_f64() * 1000.0;
+    
 
     println!("Mean: {}, Median: {}, Std Dev: {}", mean, median, std_dev);
     println!("Memory Usage: {}, Execution Time: {:?}", mem_usage_perc, execution_time);
